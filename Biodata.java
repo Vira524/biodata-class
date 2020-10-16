a95ad8721f02bb22e209bb8bd7b47bafad30371c
Vira Febrianti <virafebrianti524@gmail.com>
2020-10-16 11:42:50 +0700

    biodata class

---
+++ Biodata.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Vira Febrianti
+ */
+public class Biodata {
+    public void infoNama(){
+        System.out.println("Nama : Vira Febrianti");
+    }
+     public void infoTtl(){
+        System.out.println("Ttl : Pasuruan, 05 Februari 2004");
+    }
+    public void infoJk(){
+        System.out.println("Jk : Perempuan");
+    }
+    public void infoAgama(){
+        System.out.println("Agama : Islam");
+    }
+    public void infoAlamat(){
+        System.out.println("Alamat : Curahbanyak RT 002 RW 003 Kluwut, Wonorejo, Pasuruan, Jawa Timur");
+    }
+    public void infoTelepon(){
+        System.out.println("Telepon : 085748824104");
+    }
+    public void infoHobby(){
+        System.out.println("Hobby : Bersepeda, menonton film, bernyanyi, mendengarkan musik, memasak, fotografi");
+    }
+    public void infoSekolah(){
+        System.out.println("Sekolah : SMKN 1 Purwosari");
+    }
+    public void infoKelas(){
+        System.out.println("Kelas : XI RPL 1");
+    }
+}
---
+++ Utama.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Vira Febrianti
+ */
+public class Utama {
+    public static void main(String [] args){
+        Biodata biodata = new Biodata();
+        biodata.infoNama();
+        biodata.infoTtl();
+        biodata.infoJk();
+        biodata.infoAgama();
+        biodata.infoAlamat();
+        biodata.infoTelepon();
+        biodata.infoHobby();
+        biodata.infoSekolah();
+        biodata.infoKelas();
+    }
+}
